78128b1be9b2160d58ee71b035362ce9896c412a
PetrovD <kam1k@mail.ru>
2023-03-31 19:16:03 +0300

    added lesson 3 hw

--- blog/app.py
+++ blog/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from blog.views.users import users_app
 from blog.views.articles import articles_app
-from blog.views.auth import auth_app
+from blog.views.auth import auth_app, login_manager
 from blog.models.database import db
 
 app = Flask(__name__)
@@ -10,7 +10,10 @@ app.register_blueprint(articles_app, url_prefix="/articles")
 app.register_blueprint(auth_app, url_prefix="/auth")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "abcdefg123456"
+
 db.init_app(app)
+login_manager.init_app(app)
 
 
 @app.route("/")

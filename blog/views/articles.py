40d9c7e235dc2b7223ceb1ddc9c805b252603627
PetrovD <kam1k@mail.ru>
2023-04-20 11:51:26 +0300

    added lesson 2 hw

---
+++ blog/views/articles.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, render_template
+from werkzeug.exceptions import NotFound
+
+articles_app = Blueprint("articles_app", __name__)
+
+ARTICLES = {1:"Flask", 2:"Django", 3:"JSON:API"}
+
+
+@articles_app.route("/", endpoint="list")
+def articles_list():
+    return render_template("articles/list.html", articles=ARTICLES)
+
+
+@articles_app.route("/<int:article_id>/", endpoint="details")
+def article_details(article_id: int):
+    try:
+        article_name = ARTICLES[article_id]
+    except KeyError:
+        raise NotFound(f"User #{article_id} doesn't exist!")
+    return render_template('articles/details.html', article_id=article_id, article_name=article_name)
